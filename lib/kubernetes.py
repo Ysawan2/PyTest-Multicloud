b5cedfb9106791e51a99f5a6cfdec4937501dbe8
Sawan Y <187062279+Ysawan2@users.noreply.github.com>
2024-12-26 18:56:28 -0800

    basic structure

---
+++ lib/kubernetes.py
@@ -0,0 +1,23 @@
+
+class Kubernetes:
+    def __init__(self):
+        pass
+
+    def create_deployment(self):
+        pass
+
+    def delete_deployment(self):
+        pass
+
+    def get_deployment(self):
+        pass
+    
+    def create_service(self):
+        pass
+
+    def delete_service(self):
+        pass
+
+    def get_service(self):
+        pass
+
