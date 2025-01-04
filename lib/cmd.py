a10a0c1b471744e3fa1246a5344b9a666d6ff481
Sawan Y <187062279+Ysawan2@users.noreply.github.com>
2025-01-03 22:02:19 -0800

    additional changes

--- lib/cmd.py
+++ lib/cmd.py
@@ -1,5 +1,8 @@
 import subprocess
 
-def run(cmd):
-    output = subprocess.run(cmd, shell=True, timeout=2*60)
-    return output.stdout
+class CmdRunner:
+    def run_command(self, cmd, shell=True, timeout=2*60):
+        output = subprocess.run(cmd, shell=shell, timeout=timeout)
+        if output.stderr:
+            return output.stderr
+        return output.stdout
